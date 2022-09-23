ffca2e1a7a908ce3b66225be4324b0645525df22
ZoiaRatushna <93883758+ZoiaRatushna@users.noreply.github.com>
2022-09-23 20:37:58 +0300

    hw_task6,7

--- js/task-06.js
+++ js/task-06.js
@@ -8,11 +8,11 @@
 //   placeholder="Please enter 6 symbols"
 // />
 
-// Яка кількість смиволів повинна бути в інпуті, зазначається в 
+// Яка кількість смиволів повинна бути в інпуті, зазначається в
 // його атрибуті data - length.
-// Якщо введена правильна кількість символів, то border інпуту 
+// Якщо введена правильна кількість символів, то border інпуту
 // стає зеленим, якщо неправильна кількість - червоним.
-// Для додавання стилів використовуй CSS-класи valid і invalid, 
+// Для додавання стилів використовуй CSS-класи valid і invalid,
 // які ми вже додали у вихідні файли завдання.
 
 // #validation-input {
@@ -26,3 +26,22 @@
 // #validation-input.invalid {
 //   border-color: #f44336;
 // }
+
+let inputVal = document.getElementById("validation-input");
+
+let totalLenght = inputVal.getAttribute("data-length");
+let intTotallenght = parseInt(totalLenght, 10);
+
+inputVal.oninput = function() {
+  if (inputVal.value.length === intTotallenght) {
+    inputVal.classList.remove("invalid");
+    inputVal.classList.add("valid");
+  }
+  if (inputVal.value.length === 0) {
+    inputVal.classList.remove("valid");
+    inputVal.classList.remove("invalid");
+  }
+  if (inputVal.value.length !== intTotallenght && inputVal.value.length !== 0) {
+    inputVal.classList.add("invalid");
+  }
+};
--- js/task-07.js
+++ js/task-07.js
@@ -7,3 +7,10 @@
 // <br />
 // <span id="text">Abracadabra!</span>
 
+let controlFont = document.getElementById("font-size-control");
+
+let text = document.getElementById("text");
+
+controlFont.oninput = function() {
+  text.style.fontSize = controlFont.value + "px";
+};
