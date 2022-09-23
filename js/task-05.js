0d85e6a605b3e05cdf4e040c3db62d96c1356779
ZoiaRatushna <93883758+ZoiaRatushna@users.noreply.github.com>
2022-09-23 20:15:46 +0300

    hw_task4,5

--- js/task-05.js
+++ js/task-05.js
@@ -2,3 +2,14 @@
 // input(подія input), підставляє його поточне значення в span#name -
 // output.Якщо інпут
 // порожній, у спані повинен відображатися рядок "Anonymous".
+
+let input = document.getElementById("name-input");
+let nameOutput = document.getElementById("name-output");
+
+input.oninput = function () {
+    if (input.value === '') {
+       nameOutput.innerHTML = 'Anonymous';
+    } else{
+    nameOutput.innerHTML = input.value;
+    }
+}
