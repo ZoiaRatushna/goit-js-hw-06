d996b7e4d240d61ff2a43743d449e848328890f9
ZoiaRatushna <93883758+ZoiaRatushna@users.noreply.github.com>
2022-09-23 20:58:17 +0300

    hw_task8,9,10

--- js/task-08.js
+++ js/task-08.js
@@ -23,3 +23,22 @@
 // використовуй властивість elements.
 // Виведи об'єкт із введеними даними в консоль і очисти значення 
 // полів форми методом reset.
+
+const form = document.querySelector('.login-form')
+
+form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+    event.preventDefault()
+    const {
+        elements: { email, password },
+    } = event.currentTarget
+
+    if (email.value === '' || password.value === '') {
+        return alert('Please fill in all the fields!')
+    }
+    const userDetails = { email: email.value, Password: password.value }
+    
+    console.log(userDetails)
+    event.currentTarget.reset()
+}
